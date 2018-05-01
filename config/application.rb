939afa90e93d18107e9fadb617b27fe30f1b52dd
veejar2020 <veejar2020@gmail.com>
2018-04-30 23:46:53 -0700

    removing active record from rails

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
+
 require 'dotenv/load' unless Rails.env.production?
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
